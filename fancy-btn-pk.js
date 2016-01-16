d0112d8b49f177d0fafef13fe90db0d67637b485
Pankaj Thakur <Pankaj-Thakur@users.noreply.github.com>
2016-01-16 12:43:51 +0530

    Create fancy-btn-pk.js

---
+++ fancy-btn-pk.js
@@ -0,0 +1,2 @@
+jQuery('.cs-btn').append("<span class='lines top'></span><span class='lines right'></span><span class='lines bottom'></span><span class='lines left'></span>");
+jQuery('.cs-btn').attr('pk','');
